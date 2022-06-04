8d84a92c63d6b27111d068ee22791e490822192a
rimtimti <rimtimti@mail.ru>
2022-06-04 20:41:04 +0300

    Example 004 div+random

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+double numbrerB = new Random().Next(1, 10);
+Console.WriteLine(numbrerB);
+Console.WriteLine(numberA / numbrerB);
