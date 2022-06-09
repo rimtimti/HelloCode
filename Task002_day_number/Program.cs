f79c4065f47bad2d42a55819c40475c2d71709d1
rimtimti <rimtimti@mail.ru>
2022-06-09 19:22:19 +0300

    дни недели по числам

---
+++ Task002_day_number/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+Write("Ведите номер дня недели: ");
+int day = Convert.ToInt32(ReadLine());
+
+if ((day < 1 || day > 7)) {
+    WriteLine("Ошибка!!!");
+    return;
+}
+if (day == 1) WriteLine("Понедельник");
+if (day == 2) WriteLine("Вторник");
+if (day == 3) WriteLine("Среда");
+if (day == 4) WriteLine("Четверг");
+if (day == 5) WriteLine("Пятница");
+if (day == 6) WriteLine("Суббота");
+if (day == 7) WriteLine("Воскресенье");
