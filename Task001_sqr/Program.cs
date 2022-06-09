893d8c4cd2e4d23a626cd9c2c4d9662e23fcef76
rimtimti <rimtimti@mail.ru>
2022-06-09 19:01:25 +0300

    вычисление квадрата числа

---
+++ Task001_sqr/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+WriteLine("Введите число:");
+int number = Convert.ToInt32(ReadLine());
+//int sqr = number * number;
+double sqr = Math.Pow(number, 2);
+Write("Квадрат этого числа равен: ");
+WriteLine(sqr);
