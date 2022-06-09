4f34b5f4300576ccaaa44a58fcbdf219a6476ba6
rimtimti <rimtimti@mail.ru>
2022-06-09 22:58:30 +0300

    поиск элемента

---
+++ Example010_Methods_Array2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+int [] numbers = {233,32,44,54,36,76,89,22,36};
+
+int n = numbers.Length;
+int find = 36;
+int index = 0;
+while (index < n) {
+    if (numbers [index] == find) {
+        WriteLine (index);
+        break;
+    }
+    index++;
+}
