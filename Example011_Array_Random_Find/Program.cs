7094efad451378a8707a1b79aa7b6cec15126c7f
rimtimti <rimtimti@mail.ru>
2022-06-10 20:53:51 +0300

    поиск элемента в радномном массиве

---
+++ Example011_Array_Random_Find/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+Console.Clear();
+//метод заполнения массива рандомными числами
+void FillArray (int [] collection) {
+    int length = collection.Length;
+    int index = 0;
+    while (index < length){
+        collection [index] = new Random().Next(1,10);
+    index++;
+    }
+}
+//метод печати массива
+void PrintArray (int [] coll) {
+    int count = coll.Length;
+    int pos = 0;
+    while (pos < count){
+        Console.WriteLine(coll[pos]);
+    pos++;
+    }
+}
+//метод по поиску индекса нужного числа в массиве
+int IndexOf (int [] collection, int find){
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;//если элемента нет, то выдаст -1, а не 0 ))
+    while (index < count){
+        if (collection[index] == find){
+            position = index;
+            break;//если нужен только первый индекс, то это... без break будет последний индекс
+        }
+        index++;
+    }
+    return position;
+}
+
+int [] array = new int [10];
+FillArray (array);
+PrintArray (array);
+
+Console.WriteLine();
+
+int pos = IndexOf(array,4);
+Console.WriteLine(pos);
